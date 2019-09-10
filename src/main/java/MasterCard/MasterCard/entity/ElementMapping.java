0806e04c32c7b2fdb4e140e5e741a010196e6825
RA20092282 <rai.pallavi.1997@gmail.com>
2019-09-10 11:51:56 +0530

    Changes

--- src/main/java/MasterCard/MasterCard/entity/ElementMapping.java
+++ src/main/java/MasterCard/MasterCard/entity/ElementMapping.java
@@ -19,58 +19,11 @@ import javax.persistence.Id;
 public class ElementMapping  {
 
     @Id
-    Integer id;
-    String feeder;
-    String elements;
-    String roleName;
-    String enable;
-    String as;
+    Integer Id;
+    String Feeder;
+    String Elements;
+    String RoleName;
+    String  Enable;
+    String As;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFeeder() {
-        return feeder;
-    }
-
-    public void setFeeder(String feeder) {
-        this.feeder = feeder;
-    }
-
-    public String getElements() {
-        return elements;
-    }
-
-    public void setElements(String elements) {
-        this.elements = elements;
-    }
-
-    public String getRoleName() {
-        return roleName;
-    }
-
-    public void setRoleName(String roleName) {
-        this.roleName = roleName;
-    }
-
-    public String getEnable() {
-        return enable;
-    }
-
-    public void setEnable(String enable) {
-        this.enable = enable;
-    }
-
-    public String getAs() {
-        return as;
-    }
-
-    public void setAs(String as) {
-        this.as = as;
-    }
 }
