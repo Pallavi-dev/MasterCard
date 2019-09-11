50163cc95bd1303e966a73b9438a273a334ec7f3
RA20092282 <rai.pallavi.1997@gmail.com>
2019-09-11 12:15:00 +0530

    Merge branch 'master' of https://github.com/Pallavi-dev/MasterCard
    # Conflicts:
    #	src/main/java/MasterCard/MasterCard/controller/UserRoleController.java
    #	src/main/java/MasterCard/MasterCard/entity/RoleMapping.java
    #	src/main/java/MasterCard/MasterCard/repository/UserRoleRepository.java

--- src/main/java/MasterCard/MasterCard/entity/ElementMapping.java
+++ src/main/java/MasterCard/MasterCard/entity/ElementMapping.java
@@ -1,29 +1,35 @@
 package MasterCard.MasterCard.entity;
 
 
+import MasterCard.MasterCard.model.ElementMappingCompositeKey;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 
 @Entity
+@IdClass(ElementMappingCompositeKey.class)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 
+
 public class ElementMapping  {
 
     @Id
-    Integer id;
-    String feeder;
     String elements;
-    String roleName;
+    @Id
     String enable;
+    @Id
+    String roleName;
+    String feeder;
     String asFields;
 
 }
